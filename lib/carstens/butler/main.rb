8c3e5eff470503379af7f8d7a6f5e92658eed342
Carsten Dreesbach <carsten.dreesbach@opower.com>
2020-10-23 09:31:36 -0400

    Better logging, downcasing quetion, better bot identification

--- lib/carstens/butler/main.rb
+++ lib/carstens/butler/main.rb
@@ -1,9 +1,14 @@
 require 'http'
+require 'logger'
 require 'openssl'
 require 'sinatra'
 require 'time'
 require "pry" if development? || test?
 require "sinatra/reloader" if development?
+
+configure :development do
+  set :logging, Logger::DEBUG
+end
 
 set :bind, '0.0.0.0'
 
@@ -13,6 +18,7 @@ end
 
 def carsten_bot(question)
   question ||= ""
+  question.downcase!
   bad_clients = ["ngma","cms","tru"]
   probably_no = ["is it possible to","do you think we can","the client asked if we could","is it feasible to","can we","should we","can i","should i"]
   if bad_clients.any? { |client| question.include? client }
@@ -31,36 +37,36 @@ def carsten_bot(question)
 end
 
 post '/ask' do
-  puts "Headers: #{headers}"
-  puts "request.env: #{request.env}"
+  logger.debug "Headers: #{headers}"
+  logger.debug "request.env: #{request.env}"
   etag '', :new_resource => true
   request.body.rewind
   data = JSON.parse request.body.read
   question = data.first[1].downcase
-  puts question
+  logger.debug question
   carsten_bot(question)
 end
 
 post '/slack/challenge' do
-  puts "headers: #{headers}"
-  puts "request.env: #{request.env}"
+  logger.debug "headers: #{headers}"
+  logger.debug "request.env: #{request.env}"
   etag '', :new_resource => true
   request.body.rewind
   raw_body = request.body.read
   data = JSON.parse raw_body
   challenge = data['challenge']
-  puts "raw_body: #{raw_body}"
-  puts "data: #{data}"
-  puts "challenge: #{challenge}"
+  logger.debug "raw_body: #{raw_body}"
+  logger.debug "data: #{data}"
+  logger.debug "challenge: #{challenge}"
 
 
   timestamp = request.env['HTTP_X_SLACK_REQUEST_TIMESTAMP']
-  puts "timestamp: #{timestamp}"
+  logger.debug "timestamp: #{timestamp}"
   # The request timestamp is more than five minutes from local time.
   # It could be a replay attack, so let's ignore it.
   limit = ENV["RACK_ENV"] == "production" ? 5 : 600
   if (Time.now.to_i - timestamp.to_i).abs > 60 * limit
-    puts "Old request, ignoring!"
+    logger.debug "Old request, ignoring!"
     return
   end
   basestring = ['v0', timestamp, raw_body].join(':')
@@ -71,21 +77,25 @@ post '/slack/challenge' do
     hmac = "v0=#{OpenSSL::HMAC.hexdigest(digest, key, basestring)}"
     slack_signature = request.env['HTTP_X_SLACK_SIGNATURE']
     if hmac == slack_signature
-      puts "hooray, the request came from Slack!"
+      logger.debug "hooray, the request came from Slack!"
       return deal_with_request(data, challenge)
     else
-      puts "Failed key #{i}"
+      logger.debug "Failed key #{i}"
     end
   end
   [401, "Invalid request, X-Slack-Signature: #{request.env['HTTP_X_SLACK_SIGNATURE']}, hmac: #{hmac}"]
 end
 
 def deal_with_request(data, challenge)
-  puts "Yay!"
-  puts "data: #{data}"
-  puts "challenge: #{challenge}"
+  logger.debug "data: #{data}"
+  logger.debug "challenge: #{challenge}"
   return challenge if challenge
-  return unless data.dig("event", "bot_id").nil? # Don't reply to bots, this sends you into a loop of talking to yourself! ;]
+  return if data.dig("event", "subtype") == "bot_message" # Don't reply to bots, this sends you into a loop of talking to yourself! ;]
   post_url = ENV['POST_WEBHOOK_URL']
-  HTTP.post(post_url, :body => {"text" => carsten_bot(data.dig("event", "text"))}.to_json)
+  reply = carsten_bot(data.dig("event", "text"))
+  if ENV["RACK_ENV"] == "production"
+    HTTP.post(post_url, :body => {"text" => reply}.to_json)
+  else
+    reply
+  end
 end
